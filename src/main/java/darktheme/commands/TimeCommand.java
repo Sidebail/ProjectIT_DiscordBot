5f17bd43a41eaacd5b56fc7f4d3673d46ad0ed03
200359764 <31934766+200359764@users.noreply.github.com>
2019-06-27 19:54:47 -0400

    Add files via upload

---
+++ src/main/java/darktheme/commands/TimeCommand.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package darktheme.commands;
+
+import java.time.LocalDateTime;
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.entity.message.MessageAuthor;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+
+/**
+ *
+ * @author seunghwankim
+ */
+public class TimeCommand implements Command{
+
+    @Override
+    public void process(MessageCreateEvent event) {
+        MessageAuthor author = event.getMessageAuthor();
+        EmbedBuilder embed = new EmbedBuilder()
+                .setTitle("Barrie")
+                .addField("Time", author.LocalDateTime.now(), true)
+                .setAuthor(author);
+        
+        event.getChannel().sendMessage(embed);
+    }
+    
+}
